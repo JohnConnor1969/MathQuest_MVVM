4122d03c9342b76c1e0461de71e92707661a6213
Valeriy <Valeriy>
2020-08-16 22:45:15 +0700

    Initial commit

---
+++ MathQuest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MathQuest
+//
+//  Created by Valeriy Pokatilo on 16.08.2020.
+//  Copyright © 2020 Valeriy Pokatilo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
